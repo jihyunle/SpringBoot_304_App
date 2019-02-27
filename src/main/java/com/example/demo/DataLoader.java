809ec6dc3e7b4a817acbe82a6078c598866a5053
jihyunle <jennifer.jlee@outlook.com>
2019-02-27 11:56:16 -0500

    Added comment lines

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -11,12 +11,15 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String...strings) throws Exception{
-        Car car = new Car(1992, "Cadillac", "XM-22");
+        Car car = new Car(1992, "Cadillac", "XM");
         repository.save(car);
         car = new Car(2012, "Kia", "Soul");
         repository.save(car);
         car = new Car(2019, "Tesla", "S");
         repository.save(car);
+        car = new Car(2018, "Hyundai", "SantaFe");
+        repository.save(car);
+
     }
-    
+
 }
