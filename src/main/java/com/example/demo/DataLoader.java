4a9557564c729a3503aef4e39d5cd06a3e87b6fe
jihyunle <jennifer.jlee@outlook.com>
2019-02-27 11:50:39 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    CarRepository repository;
+
+    @Override
+    public void run(String...strings) throws Exception{
+        Car car = new Car(1992, "Cadillac", "XM-22");
+        repository.save(car);
+        car = new Car(2012, "Kia", "Soul");
+        repository.save(car);
+        car = new Car(2019, "Tesla", "S");
+        repository.save(car);
+    }
+    
+}
